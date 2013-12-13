d9141c0719c90c932843d044cbb8a678aa86ef6d
James Liao <grittathh@gmail.com>
2013-12-13 16:27:38 -0500

    updates

--- createScratchEnvironmentPFS.m
+++ createScratchEnvironmentPFS.m
@@ -17,9 +17,13 @@ system('cp $PBS_O_WORKDIR/inputDataStruct.mat .','-echo');
 system('rm *.ndx','-echo');
 system('touch fileTracker.ndx','-echo');
 
-load('inputDataStruct.mat')
+matObj = matfile('inputDataStruct.mat');
+if(sum(theSize == 1) == 1)
+    maxDirectoryNumber = ceil(max(theSize) / 1000);
+else
+    maxDirectoryNumber = ceil(theSize(2)/1000);
+end
 
-maxDirectoryNumber = ceil(length(inputDataStruct)/1000);
 directoryCreationString = '';
 for(index = 1:maxDirectoryNumber)
     directoryCreationString = [directoryCreationString ' ' num2str(index)];
--- startWorkerLogic.sh
+++ startWorkerLogic.sh
@@ -1,19 +1,27 @@
 #!/bin/bash -login
 export PATH=$PATH:$PBS_O_PATH
 echo 'inside individual worker'
-echo ${PBS_O_WORKDIR}
-echo ${OWORKDIR}
-echo ${SCRATCHDIR}
+
+if [ $USEPBSDSH -eq 1 ]
+then
+echo 'yes pbsdsh'
+let PBS_VNODENUM=$PBS_VNODENUM+$MY_VNODENUM
+export TMPDIR=${SCRATCHDIR}/scratch${PBS_VNODENUM}
+else
+echo  'no pbsdsh'
+let PBS_VNODENUM=$MY_VNODENUM+1
+fi
+
+
 echo ${PBS_VNODENUM}
 
-let PBS_VNODENUM=$PBS_VNODENUM+1
-
-echo ${PBS_VNODENUM}
+#printenv
 
 echo "${SCRATCHDIR}/scratch${PBS_VNODENUM}"
+echo ${TMPDIR}
 
 cd ${OWORKDIR}
-matlab -nodisplay -nosplash -r "myWorkerLogic(${PBS_VNODENUM},'${SCRATCHDIR}')" > \
+matlab -nodisplay -nosplash -r "myWorkerLogic(${PBS_VNODENUM},'${SCRATCHDIR}'); exit" > \
 "${SCRATCHDIR}/scratch${PBS_VNODENUM}/out${PBS_VNODENUM}.out"
 
 echo "myWorkerLogic function has returned"
